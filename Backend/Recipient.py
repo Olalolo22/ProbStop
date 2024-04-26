ff6af9debc86cc8ba5b05139f9cb758308e71fd0
elguion07 <114438121+elguion07@users.noreply.github.com>
2024-04-26 13:06:03 +0100

    Create Recipient.py
    Models a potential Recipient of an Organ

---
+++ Backend/Recipient.py
@@ -0,0 +1,17 @@
+class Recipient:
+    def __init__(self, name, age, height, weight, blood_group, hla_typing, degree_of_ailment):
+        self.name = name
+        self.age = age
+        self.height = height 
+        self.weight = weight  
+        self.blood_group = blood_group
+        self.hla_typing = hla_typing 
+        self.degree_of_ailment = degree_of_ailment  
+
+    def calculate_bmi(self):
+        height_m = self.height / 100
+        bmi = self.weight / (height_m ** 2)
+        return bmi
+
+    def get_hla_alleles(self):
+        return [tuple(allele.split(':')) for allele in self.hla_typing.split(',')]
