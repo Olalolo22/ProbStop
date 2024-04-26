4b96ef67ad45ec766ced8eb7be29df76cffd66a4
elguion07 <114438121+elguion07@users.noreply.github.com>
2024-04-26 14:51:01 +0100

    Update Donor.py features

--- Backend/Donor.py
+++ Backend/Donor.py
@@ -1,5 +1,5 @@
 class Donor:
-    def __init__(self, name, age, height, weight, blood_group, hla_typing, nature_of_ailment, cause_of_ailment):
+    def __init__(self, name, age, height, weight, blood_group, hla_typing, nature_of_ailment):
         self.name = name
         self.age = age
         self.height = height  # in centimeters
@@ -7,7 +7,7 @@ class Donor:
         self.blood_group = blood_group
         self.hla_typing = hla_typing  # comma-separated string of HLA alleles
         self.nature_of_ailment = nature_of_ailment
-        self.cause_of_ailment = cause_of_ailment
+        
 
     def calculate_bmi(self):
         height_m = self.height / 100
