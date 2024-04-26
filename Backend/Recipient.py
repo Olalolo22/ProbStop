cac391ffceba8db8b4d54e6f742cb31ea2f1121d
elguion07 <114438121+elguion07@users.noreply.github.com>
2024-04-26 14:48:48 +0100

    Update Recipient.py features

--- Backend/Recipient.py
+++ Backend/Recipient.py
@@ -1,12 +1,12 @@
 class Recipient:
-    def __init__(self, name, age, height, weight, blood_group, hla_typing, degree_of_ailment):
+    def __init__(self, name, age, height, weight, blood_group, hla_typing, nature_of_ailment):
         self.name = name
         self.age = age
         self.height = height 
         self.weight = weight  
         self.blood_group = blood_group
         self.hla_typing = hla_typing 
-        self.degree_of_ailment = degree_of_ailment  
+        self.nature_of_ailment = nature_of_ailment  
 
     def calculate_bmi(self):
         height_m = self.height / 100
