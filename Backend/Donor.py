c1db2f26d642df041902464ac69651bf10911711
elguion07 <114438121+elguion07@users.noreply.github.com>
2024-04-26 13:03:05 +0100

    Create Donor.py

---
+++ Backend/Donor.py
@@ -0,0 +1,18 @@
+class Donor:
+    def __init__(self, name, age, height, weight, blood_group, hla_typing, nature_of_ailment, cause_of_ailment):
+        self.name = name
+        self.age = age
+        self.height = height  # in centimeters
+        self.weight = weight  # in kilograms
+        self.blood_group = blood_group
+        self.hla_typing = hla_typing  # comma-separated string of HLA alleles
+        self.nature_of_ailment = nature_of_ailment
+        self.cause_of_ailment = cause_of_ailment
+
+    def calculate_bmi(self):
+        height_m = self.height / 100
+        bmi = self.weight / (height_m ** 2)
+        return bmi
+
+    def get_hla_alleles(self):
+        return [tuple(allele.split(':')) for allele in self.hla_typing.split(',')]
